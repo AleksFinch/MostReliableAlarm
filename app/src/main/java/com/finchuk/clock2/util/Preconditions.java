6a969afc3ca88bdc1b3b017d4b01556542a20dcb
Oleksandr Finchuk <dragonbest1997@gmail.com>
2017-06-07 01:32:15 +0300

    Alarm and menu configuration was added

---
+++ app/src/main/java/com/finchuk/clock2/util/Preconditions.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 Oleksandr Finchuk
+ *
+ * This file is part of ClockPlus.
+ *
+ * ClockPlus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ClockPlus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ClockPlus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.finchuk.clock2.util;
+
+/**
+ * Created by Oleksandr Finchuk on 07/06/2017.
+ */
+public final class Preconditions {
+    private Preconditions() {}
+
+    public static <T> T checkNotNull(T obj) {
+        if (null == obj)
+            throw new NullPointerException();
+        return obj;
+    }
+}
